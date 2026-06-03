f4894f6dbfe0da10c8b526d76743844ca98ef497
Ephraim Immanuel <ephraimimmanuel@karunya.edu.in>
2026-06-03 16:35:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hi, I finished my Frontend Deployment Project 🚀</h1>
+      <h2>GitHub → Jenkins → S3</h2>
+    </div>
+  );
+}
+
+export default App;
