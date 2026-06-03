4aea922a53abbd577eb4725d26e947b93662ba3f
Ephraim Immanuel <ephraimimmanuel@karunya.edu.in>
2026-06-03 19:09:46 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Hi, I finished my Frontend Deployment Project 🚀</h1>
+      <h1>Hi i am Ephraim, I finished my Frontend Deployment Project 🚀</h1>
       <h2>GitHub → Jenkins → S3</h2>
     </div>
   );
